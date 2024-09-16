e119950444894050be7d3f4cecd45f8150cdc38c
Mia Winter <mia@miawinter.de>
2024-09-16 13:44:35 +0200

    fixed window startup size and position on windows

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,4 +6,18 @@ public partial class App : Application {
 
 		MainPage = new AppShell();
 	}
+
+	protected override Window CreateWindow(IActivationState? activationState) {
+		var window = base.CreateWindow(activationState);
+		
+		window.MinimumWidth = 350;
+		window.MinimumHeight = 450;
+
+		window.Width = 400;
+		window.Height = 700;
+		window.X = 10;
+		window.Y = 10;
+
+		return window;
+	}
 }
